a4fc0c0dbdd623f15ed1e7c27469a3dcb83d3505
DaGoja <lo.grosjean1@gmail.com>
2026-04-07 11:57:09 +0200

    Ajout des accordéons dans la faq

---
+++ js/app.js
@@ -0,0 +1,7 @@
+<script>
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = 
