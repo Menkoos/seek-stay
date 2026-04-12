7ee8db17b045eaae1af7dadcae9567ec14c6ce8f
Kylian Enyegue <kylian.enyeguemvodo@gmail.com>
2026-04-12 17:24:34 +0200

    ok ajout et correctionformualire contact mais probleme avec php et myadmin

--- php/contact.php
+++ php/contact.php
@@ -1,30 +1,90 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ton_database");
+// define variables and set to empty values
+$nomErr = $emailErr = $telephoneErr = $genderErr = $typeErr = "";
+
+$nom = $email = $telephone = $message = $gender = $typeDemande = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $nom = test_input($_POST['nom']);
-    $email = test_input($_POST['email']);
-    $telephone = test_input($_POST['telephone']);
-    $message = test_input($_POST['message']);
-    $genre = $_POST['gender'];
-    $typeDemande = $_POST['typeDemande'];
+  // NOM
+  if (empty($_POST["nom"])) {
+    $nomErr = "Nom requis";
+  } else {
+    $nom = test_input($_POST["nom"]);
+  }
 
+  // EMAIL
+  if (empty($_POST["email"])) {
+    $emailErr = "Email requis";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+
+  // TELEPHONE
+  if (empty($_POST["telephone"])) {
+    $telephoneErr = "Téléphone requis";
+  } else {
+    $telephone = test_input($_POST["telephone"]);
+  }
+
+  // MESSAGE (optionnel)
+  if (empty($_POST["message"])) {
+    $message = "";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
+
+  // GENRE
+  if (empty($_POST["gender"])) {
+    $genderErr = "Genre requis";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+
+  // TYPE DEMANDE
+  if (empty($_POST["typeDemande"])) {
+    $typeErr = "Type de demande requis";
+  } else {
+    $typeDemande = test_input($_POST["typeDemande"]);
+  }
+
+  // INSERT si aucun champ obligatoire vide
+  if (
+    $nomErr == "" &&
+    $emailErr == "" &&
+    $telephoneErr == "" &&
+    $genderErr == "" &&
+    $typeErr == ""
+  ) {
+
+    $conn = new mysqli("localhost", "root", "", "ton_database");
 
     $sql = "INSERT INTO contact (nom, email, telephone, genre, type_demande, message)
-            VALUES ('$nom', '$email', '$telephone', '$genre', '$typeDemande', '$message')";
+            VALUES ('$nom', '$email', '$telephone', '$gender', '$typeDemande', '$message')";
 
     $conn->query($sql);
+    $conn->close();
+  }
 }
+?> 
 
+<?php
+echo "<h2>DEBUG - Your Input:</h2>";
 
+echo "Nom: " . $nom;
+echo "<br>";
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+echo "Email: " . $email;
+echo "<br>";
 
-$conn->close();
+echo "Telephone: " . $telephone;
+echo "<br>";
+
+echo "Message: " . $message;
+echo "<br>";
+
+echo "Genre: " . $gender;
+echo "<br>";
+
+echo "Type de demande: " . $typeDemande;
 ?>
